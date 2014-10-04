5d4c5dade3515189ff8d3d082689c00f44a07b5e
Avinodh <Ashvin@calvisitor-10-105-129-62.calvisitor.1918.berkeley.edu>
2014-10-04 09:32:29 -0700

    database add

---
+++ fetchpets.php
@@ -0,0 +1,39 @@
+<?
+
+$username = "root";
+$password = "root";
+$database = "tindog";
+$con = mysql_connect(localhost,$username,$password);
+if(!$con)
+{
+    die("Could not connect :".mysql_error());
+}
+
+mysql_select_db($database);
+
+
+$fetch = "SELECT * FROM user";
+$result = mysql_query($fetch);
+$n_rows = mysql_num_rows($result);
+
+$return_arr = array();
+
+
+for($i=0; $i < $n_rows; $i++)
+{
+    $row['pet_name'] = mysql_result($result, $i, "dog_name");
+    $row['pet_age'] = mysql_result($result, $i, "dog_age");
+    $row['pet_image']= mysql_result($result, $i, "dog_image");
+    $row['pet_breed']= mysql_result($result, $i, "dog_breed");
+    $row['pet_type'] = mysql_result($result, $i, "dog_type");
+    $row['comments'] = mysql_result($result, $i, "comments");
+    $row['contact'] = mysql_result($result, $i, "contact");  
+
+    array_push($return_arr,$row);
+}
+
+mysql_close();
+echo json_encode($return_arr,JSON_UNESCAPED_SLASHES);
+
+
+?> 
---
+++ putdata.php
@@ -0,0 +1,47 @@
+<?
+$username = "root"; 
+$password = "root"; 
+$database = "tindog"; 
+
+$owner = $_POST["owner"];
+$pet_age = $_POST["pet_age"];
+$pet_name = $_POST["pet_name"];
+$pet_type = $_POST["pet_type"];
+$pet_breed = $_POST["pet_breed"];
+$comments = $_POST["comments"];
+$contact = $_POST["contact"];
+
+$owner_image = ($_FILES['user_image']['name']); 
+$pet_image = ($_FILES['pet_image']['name']); 
+
+
+$target = 'images/';
+/*
+move_upload_file($_FILES['user_image']['tmp_name'],$uploads_dir);
+move_upload_file($_FILES['pet_image']['tmp_name'],$uploads_dir);*/
+
+
+$con = mysql_connect(localhost,$username,$password); 
+if(!$con)
+{
+	die("Could not connect :".mysql_error());
+}
+
+mysql_select_db($database); 
+
+
+#$query = "CREATE TABLE contacts (id int(6) NOT NULL auto_increment,first varchar(15) NOT NULL,last varchar(15) NOT NULL,phone varchar(20) NOT NULL,mobile varchar(20) NOT NULL,fax varchar(20) NOT NULL,email varchar(30) NOT NULL,web varchar(30) NOT NULL,PRIMARY KEY (id),UNIQUE id (id),KEY id_2 (id))";
+
+$query = "INSERT INTO user VALUES('','$pet_name','$pet_age', '','','$owner','$pet_breed','$pet_type', '$comments', '$contact', '$pet_image', '$owner_image')";
+
+$success = mysql_query($query);
+
+if(!$success){
+	die("Error inserting data!"); 
+}
+
+move_uploaded_file($_FILES['user_image']['tmp_name'], $target.$_FILES['user_image']['name']);
+move_uploaded_file($_FILES['pet_image']['tmp_name'], $target.$_FILES['pet_image']['name']);
+ 
+mysql_close(); 
+?>
